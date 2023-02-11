52743212306531143600e9001d3c17ed635938f5
sunny8750 <120244832+sunny8750@users.noreply.github.com>
2023-02-11 16:12:58 +0530

    Update script.js
    sol

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /** @format */
 
-let arr = [
+var arr = [
   { id: 1, name: "john", age: "18", marks: 80 },
   { id: 2, name: "jack", age: "20", marks: 85 },
   { id: 3, name: "karen", age: "19", marks: 35 },
@@ -8,20 +8,49 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  var newArr=arr.map(item=>{
+    return{
+        id:item.id,
+        name:item.name,
+        age :item.age,
+        marks:item.marks
+      }
+    })
+    console.log(newArr);
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  var a =[];
+  a = arr.forEach((index) => {
+   console.log([`id:${index.id} name:${index.name} age:${index.age} marks:${index.marks}`]);
+});
 }
+
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:4,name:"susan",age:"20",marks:45}) //taking the  example of given by  acciojob in question
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].marks < 50) {
+      arr.splice(i);
+      break;
+    }
+  }
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  var arr2 = [
+    { id: 4, name: "abhi", age: "21", marks: 08 },
+    { id: 5, name: "aaksh", age: "20", marks: 45 },
+    { id: 6, name: "avinash", age: "24", marks: 55 },
+  ];
+  console.log(arr.concat(arr2))
 }
